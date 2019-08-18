ebb3de77432f56ef08dba4f2813f7d57beaf66b3
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-08-17 20:15:15 -0400

    Part one of MVP done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
+    {/* Only one navigation tag is needed -- there is no need to close it off! */}
     <Navigation />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
